b7c1e2bd847b59c94aaed853247e6001d99e2310
loic-roux-404 <loic.roux.404@gmail.com>
2022-02-25 00:37:34 +0100

    finished logic

--- SpecFlowRental.Specs/Steps/RentalStepDefinitions.cs
+++ SpecFlowRental.Specs/Steps/RentalStepDefinitions.cs
@@ -12,6 +12,7 @@ namespace SpecFlowRental.Specs.Steps
     [Binding]
     public sealed class RentalStepDefinitions
     {
+        const string ValidationMsgFailed = "Failed user validation :";
         private readonly RentalBook _rentalBook;
 
         private List<Car> _cars;
@@ -29,11 +30,35 @@ namespace SpecFlowRental.Specs.Steps
         public void GivenFollowingCars(Table table)
         {
             _cars = table.CreateSet<Car>().ToList();
+            Console.Write(_cars.Count);
         }
+
         [Given(@"following users")]
         public void GivenFollowingUsers(Table table)
         {
            _users = table.CreateSet<User>().ToList();;
+        }
+
+        [When("login with user (.*) with exception message (.*)")]
+        public void LoginWithUser(int id, string exception = "") {
+            var u = _users.Find(usr => usr.Id == id);
+            if (exception == "") {
+                _rentalBook.Login(u.Name, u.Pass);
+
+                return;
+            };
+
+            _rentalBook.Invoking(y => y.Login(u.Name, u.Pass))
+                .Should().Throw<ValidateUserException>()
+                .WithMessage(ValidationMsgFailed + exception);
+        }
+
+        [When("register with users")]
+        public void RegisterUsers(Table table) {
+            foreach (var usrName in table.Header) {
+                var u = _users.Find(usr => usr.Name == usrName);
+                _rentalBook.Register(u.Id, u.Name, u.BornDate.ToString(), u.DriverLicense, u.Pass);
+            }
         }
     }
 }
--- SpecFlowRental/RentalBook.cs
+++ SpecFlowRental/RentalBook.cs
@@ -11,7 +11,10 @@ namespace SpecFlowRental
         public Car Car { get; set; }
 
         public DateTime Start { get; set; }
-        public DateTime Stop { get; set; }
+        public DateTime End { get; set; }
+        public double Price { get; set; }
+
+        public bool Closed {get; set;} = false;
     }
 
     public class ValidateUserException : System.Exception
@@ -23,22 +26,24 @@ namespace SpecFlowRental
     public class RentalBook
     {
         const double BasePrice = 30.00;
+
+        const string ExceedKmErrMsg = "Car not gived back on time, need to adjust price with exceeded km, please retry."
         protected List<Rental> l { get; set; }
 
-        /// <summary> Price for a car concerned in a rental </summary>
-        protected Dictionary<Car, double> RentalPrices;
+        /// <summary> Catalog with is inside rental state </summary>
+        protected Dictionary<Car, bool> catalog;
         /// <summary> User and logged state mapping </summary>
         protected Dictionary<User, Boolean> users;
 
         public void AddCar(Car car, double price)
         {
-            RentalPrices.Add(car, price);
+            catalog.Add(car, false);
         }
 
         // Possible other criterias
         #nullable enable
         public Car? SearchCar(string brandName) {
-            return new List<Car>(RentalPrices.Keys).Find(c => {
+            return new List<Car>(catalog.Keys).Find(c => {
                 if (c.Brand == brandName) {
                     return true;
                 }
@@ -48,7 +53,7 @@ namespace SpecFlowRental
         }
         #nullable disable
 
-        public void Rent(User u, Car c, double estimatedDistance)
+        public void Rent(User u, Car c, DateTime endDate, double estimatedDistance)
         {
             Validate("Is logged or registered", (user, _) => {
                 return users.GetValueOrDefault(user) == true;
@@ -87,15 +92,23 @@ namespace SpecFlowRental
             r.User = u;
             r.Car = c;
             r.Start = DateTime.Now;
+            r.End = endDate;
+            r.Price = BasePrice + (c.KmPrice * estimatedDistance);;
             l.Add(r);
-
+            catalog.Add(r.Car, true);
         }
-        public void GiveBack(Rental r)
+        public void GiveBack(Rental r, double? exceedKm = null)
         {
-            // TODO create a function that find a rental and stop it
-            // Find rental in agenda and use date
-            // test if milisecond are > to estimated
-            // if (d.Millisecond > rentalEndDate)
+            if (DateTime.Now >= r.End) {
+                if (exceedKm == null) throw new Exception(ExceedKmErrMsg);
+
+                var notNullExceedKm = (double) exceedKm;
+                r.Price += r.Car.KmPrice * notNullExceedKm;
+                r.End = DateTime.Now;
+            }
+
+            r.Closed = true;
+            catalog.Add(r.Car, false);
         }
 
         public void Login(string name, string pass)
@@ -109,7 +122,7 @@ namespace SpecFlowRental
         }
 
         public void Register(int id,
-            string name, string bornDate, int driverLicense, string pass)
+            string name, string bornDate, int? driverLicense, string pass)
         {
             try {
                 var bornD = DateTime.ParseExact(bornDate, "D", new CultureInfo("fr-FR"));
@@ -142,7 +155,7 @@ namespace SpecFlowRental
             var ulist = new List<User>(users.Keys);
 
             if (ulist.Exists(cu => cu.Name == name)) {
-                throw new Exception(name + " not exists in app", );
+                throw new Exception(name + " not exists in app");
             }
 
             return ulist.Find(cu => cu.Name == name);
